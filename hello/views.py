4ee718390195bba405049f073ed92e981eda5fdd
Tanner Kvarfordt <tanner.kvarfordt@aggiemail.usu.edu>
2017-03-31 18:30:24 +0000

    Got vist count working

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import time
+from .models import PageCount
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world." + time.strftime('%c'))
+    row, create = PageCount.objects.get_or_create(page='index')
+    row.count += 1
+    row.save()
+    
+    return HttpResponse("Hello,  at visitor #"+ str(row.count) + " " + time.strftime('%c'))
+    #visitor #"+ str(row.count) + "
+    
